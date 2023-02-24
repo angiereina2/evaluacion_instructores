f22a447076bcfa2aa194a2decbe7157136a7371a
Yennyra2407 <117462486+Yennyra2407@users.noreply.github.com>
2023-02-24 15:40:49 -0500

    Add files via upload

--- src/controlador/crud.js
+++ src/controlador/crud.js
@@ -14,4 +14,17 @@ exports.InicioL = (req, res)=>{
 
     })
 
+};
+
+exports.update = (req, res)=> {
+    const id = req.body.id;
+    const txtemail = req.body.txtemail;
+    const txtpassword = req.body.txtpassword;
+    connection.query('UPDATE Usuarios SET ? where id= ?'[{txtarea:txtarea, txtpassword:txtpassword}, id], (eror,results)=>{
+        if(error){
+            console.log(error);
+         } else{
+            res.redirect('/');
+         }
+    })
 }
--- src/vista/router.js
+++ src/vista/router.js
@@ -10,7 +10,7 @@ router.get('/',(req, res)=>{
             throw error;
         } else 
         {
-            res.send('index', {results:results});
+            res.render('index', {results:results});
         }  
     })       
 });
@@ -20,7 +20,23 @@ router.get('/create', (req,res)=>{
     res.render('create');
 })
 
+//editar
+
+router.get('/actualizar/:id', (req, res)=>{
+    connection.query('select * from Usuarios where id=?')
+    const id= req.params.id;
+    connection.query ('select * from Usuario where id=?',[id], (error, results)=> {
+        if(error){
+            throw error;
+        } else 
+        {
+            res.render('actualizar', {Usuario:results[0]});   
+        }
+})
+
+
 const crud = require ('./controlador/crud');
-router.post('/InicioL', crud.InicioL)
+router.post('/InicioL', crud.InicioL);
+router.post('/actualizar', crud.Update);
 
 module.exports = router;
